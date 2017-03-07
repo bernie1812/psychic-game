0bcd9980b30aa39519c126f1ad65dca83d8f4206
Andrew Bernstein <andy_bernstein@yahoo.com>
2017-03-07 03:28:58 -0500

    finished version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,14 +10,64 @@
 //second function compare...if/else
 // third reset function
 //console.log every step
-var options = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var wins = 0;
-var losses = 0;
-var userGuesses = 0;
-var guessesLeft = 10;  
+// var options = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+// var wins = 0;
+// var losses = 0;
+// var userGuesses = 0;
+// var guessesLeft = 10;  
 
-// here we are capturing users input
-document.onkeyup = function() {
-	var userGuesses=String.fromCharCode(event.keyCode).toLowerCase();
+// // here we are capturing users input
+// document.onkeyup = function() {
+// 	var userGuesses=String.fromCharCode(event.keyCode).toLowerCase();
 
-	console.log(userGuesses);
+// 	console.log(userGuesses);
+var options=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var wins = 0;
+  var losses=0;
+  var guessesLeft=10;  
+  var guessesSoFar=[];
+  //Capture user key click
+
+ 	document.onkeyup=function() {
+ 		var userGuess=String.fromCharCode(event.keyCode).toLowerCase();
+ 		guessesSoFar.push(userGuess);
+ 		document.querySelector('#guessesSoFar').innerHTML=guessesSoFar;
+
+ 		console.log(userGuess);
+    
+    //random letter chosen from options array
+
+  		var computerGuess=options[Math.floor(Math.random()*options.length)];
+
+  		console.log(computerGuess); 
+
+      //compare users guess to computers guess
+
+      //If user guesses the computer guess, wins are increased by 1 and # of guesses is reset to 10 - if not,# of guesses left is decreased by 1 until reaching zero then losses increased by 1 and guesses reset to 10
+
+      if(userGuess===computerGuess){
+      	wins++;
+      	console.log(wins);
+      	guessesLeft=10;
+      	console.log(guessesLeft);
+      }
+
+      else {
+      	if(guessesLeft>0){
+	       guessesLeft--;
+	      	console.log(guessesLeft);
+	      }
+	      else{
+	     	losses++;
+	      	console.log(losses);
+	        guessesLeft=10;
+	        console.log(guessesLeft);
+	      }
+
+	    }
+
+	     var html="<p>Guess what letter I am thinking of </p><p>Wins: "+wins +"</p><p>Losses:"+losses +"</p>" + "<p> Guesses Left:"+guessesLeft+"</p>";
+
+	     document.querySelector('#game').innerHTML=html;
+
+    }
